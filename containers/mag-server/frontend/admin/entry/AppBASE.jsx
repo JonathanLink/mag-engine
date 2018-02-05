aecb1a17c05cad7cd1f269f62cc321c965375de5
Jonathan Link <jonathanlink.email@gmail.com>
2018-02-05 11:10:44 +0100

    add password for admin

--- containers/mag-server/frontend/admin/entry/AppBASE.jsx
+++ containers/mag-server/frontend/admin/entry/AppBASE.jsx
@@ -71,44 +71,46 @@ class App extends Component {
         const navClass = (this.state.isNavBarHidden) ? {top: "-7rem", transition: "top 0.5s"} : {top: "0rem", transition: "top 0.5s"}
  
         return (
-            <Router>
-                <div>
-                    <Navbar app fixed position="top" style={navClass}> 
-                        <Nav>
-                            <NavbarItem><Heading size="xlarge" className="heading-app-name" >@@APP_NAME@@ - admin</Heading></NavbarItem>
-                        </Nav>
-                        <NavbarSection pullRight>
-                            <NavbarItem onClick={this.toggleMenu} style={{fontSize: "2rem", position:"relative", top:"-1rem"}}><IconMenu/></NavbarItem>
-                        </NavbarSection>
-                        <Nav className="second-navbar" horizontal style={ {display: (this.state.isBackButtonVisible) ? "block" : "none" } }>
-                            <NavbarItem onClick={ this.goBack }><IconArrowback /></NavbarItem>
-                        </Nav>
-                    </Navbar>
-                    <div className="container" style={{marginTop: (this.state.isBackButtonVisible) ? "7.5em" : "4.5em" }}>
-                        <div className={menuClass} >
-                            <div className="Drawer__container">
-                                <div className="Drawer__header">
-                                <span className="Drawer__close" onClick={this.toggleMenu}>
-                                    <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
-                                    <path d="M0 0h24v24H0z" fill="none"/>
-                                    </svg>
-                                </span>
-                                </div>
-                                <div className="Drawer__content">
-                                    <List size="large">
-                                        { /*<ListItem><Link onClick={this.toggleMenu} to={'/'}><b>Home</b></Link></ListItem>*/ }
-                                        //@AUTO-GENERATED-MENU@
-                                    </List>
-                                </div>
+        
+            <div>
+                <Navbar app fixed position="top" style={navClass}> 
+                    <Nav>
+                        <NavbarItem><Heading size="xlarge" className="heading-app-name" >@@APP_NAME@@ - admin</Heading></NavbarItem>
+                    </Nav>
+                    <NavbarSection pullRight>
+                        <NavbarItem onClick={this.toggleMenu} style={{fontSize: "2rem", position:"relative", top:"-1rem"}}><IconMenu/></NavbarItem>
+                    </NavbarSection>
+                    <Nav className="second-navbar" horizontal style={ {display: (this.state.isBackButtonVisible) ? "block" : "none" } }>
+                        <NavbarItem onClick={ this.goBack }><IconArrowback /></NavbarItem>
+                    </Nav>
+                </Navbar>
+                <div className="container" style={{marginTop: (this.state.isBackButtonVisible) ? "7.5em" : "4.5em" }}>
+                    <div className={menuClass} >
+                        <div className="Drawer__container">
+                            <div className="Drawer__header">
+                            <span className="Drawer__close" onClick={this.toggleMenu}>
+                                <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
+                                <path d="M0 0h24v24H0z" fill="none"/>
+                                </svg>
+                            </span>
+                            </div>
+                            <div className="Drawer__content">
+                                <List size="large">
+                                    { /*<ListItem><Link onClick={this.toggleMenu} to={'/'}><b>Home</b></Link></ListItem>*/ }
+                                    //@AUTO-GENERATED-MENU@
+                                </List>
                             </div>
                         </div>
-                        {/*<AnimatedSwitch  
-                            atEnter={{ opacity: 0 }}
-                            atLeave={{ opacity: 0 }}
-                            atActive={{ opacity: 1 }}
-                            className="switch-wrapper"
-                        >*/}
+                    </div>
+                    {/*<AnimatedSwitch  
+                        atEnter={{ opacity: 0 }}
+                        atLeave={{ opacity: 0 }}
+                        atActive={{ opacity: 1 }}
+                        className="switch-wrapper"
+                    >*/}
+
+                    <Router>
                         <Switch>
 
                             <Route exact path='/'  
@@ -123,9 +125,11 @@ class App extends Component {
 
                         {/*</AnimatedSwitch>*/}
                         </Switch>
-                    </div>
-                </div> 
-            </Router>
+                    </Router>
+                </div>
+                
+            </div> 
+           
         )
   }
 }
